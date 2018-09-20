f811b510a98f66546357e43af8476dc60be4ce12
ashpujari <ashpujari@hotmail.com>
2018-09-20 19:50:50 +1000

    Add php file
    add product

---
+++ salepoint/add.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="container">
+    <header class="header">
+      <a class="link-title" href="index.php">SalePoint</a>
+    </header>
+    <div class="main">
+      <a href="view.html" class="button">VIEW</a>
+      <a href="manger.html" class="button">Report</a>
+      <a href="add.php" class="button">ADD</a>
+      <a href="edit.html" class="button">EDIT</a>
+    </div>
+    <form>
+      Product ID:<br>
+      <input type="text" name="prod_id">
+      <br>Product Name:<br>
+      <input type="text" name="prod_name">
+      <br>Qty<br>
+      <input type="text" name="prod_qty">
+      <br>Price Per Item:<br>
+      <input type="text" name="prod_price">
+      <br>Product category:<br>
+      <input type="text" name="prod_category">
+      <br>Product cost price:<br>
+      <input type="text" name="prod_cost">
+      <br>Product Description:<br>
+      <input type="text" name="prod_desc">
+      <br>Product normal price:<br>
+      <input type="text" name="prod_Normal_price">
+      <br>Product Stock Order:<br>
+      <input type="text" name="prod_Normal_price">
+      <form>
+
+
+    <footer>
+      &copy; 2030 SalePoint &nbsp; <span class="separator">|</span> Design by GROUP 03
+    </footer>
+   </div>
+</body>
+</html>
