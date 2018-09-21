324bff5710433ede089ce1318ea793100769c4d4
wassbooboo <100590129@student.swin.edu.au>
2018-09-21 22:40:05 +1000

    Edit Sales- Changed display

---
+++ salepoint/edit.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/scripts.js"></script>
+</head>
+<body>
+   <div class="container">
+      <header class="header">
+          
+          
+          <a class="link-title" href="index.php">SalePoint</a>
+          
+                    
+      </header>
+    
+       
+       <ul id="nav">
+              
+                  <li><a href="view.html">Display</a></li>
+                  <li><a href="manger.html">Add</a></li>
+                  <li><a href="add.html">Edit</a></li>
+                  <li><a href="edit.html">Delete</a></li>
+        </ul>
+           
+          
+           
+           
+       
+       
+      <footer>
+          
+          &copy; 2030 SalePoint &nbsp; <span class="separator">|</span> Design by GROUP 03 
+          
+      </footer> 
+       
+       
+   </div>
+    
+</body>
+</html>
