109c55b09348b095e43dc8499573d4f4287d24f1
wassbooboo <100590129@student.swin.edu.au>
2018-09-21 22:28:52 +1000

    Edit Sales- changed the display nav

---
+++ salepoint/edit.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/scripts.js"></script>
+</head>
+<body>
+   <div class="container">
+      <header class="header">
+          
+          
+          <a class="link-title" href="index.php">SalePoint</a>
+          
+                    
+      </header>
+       
+       <ul id="nav">
+          <li><a class="active" href="#home">Display</a></li>
+          <li><a href="#news">Add</a></li>
+          <li><a href="#contact">Edit</a></li>
+          <li><a href="#about">Delete</a></li>
+        </ul>
+           
+          
+           
+           
+       
+       
+      
+       
+       
+   </div>
+    
+</body>
+</html>
