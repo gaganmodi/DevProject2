f659c1e05d638145796b9a1cc459e9705d9bf674
wassbooboo <100590129@student.swin.edu.au>
2018-09-23 22:19:03 +1000

    Edit Sales- Php Database connection

--- salepoint/edit.php
+++ salepoint/edit.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <title>Home</title>
     <link rel="stylesheet" href="css/style.css">
-    <script src="js/scripts.js"></script>
 </head>
 <body>
    <div class="container">
@@ -22,16 +21,35 @@
           <li><a href="#contact">Edit</a></li>
           <li><a href="#about">Delete</a></li>
         </ul>
-           
-          
-           
-           
-       
-       
-      
-       
-       
+  
    </div>
-    
+<?php
+    $DBConnect = @mysqli_connect("feenix-mariadb.swin.edu.au", "s100590129","301196", "s100590129_db")
+        Or die ("<p>Unable to connect to the database server.</p>". "<p>Error code ". mysqli_connect_errno().": ". mysqli_connect_error()). "</p>";
+    $SQLstring = "SELECT item_id, item_name, item_desc, item_normalprice, item_currentprice, item_cost, item_stock, item_stockorder, item_category  FROM PHPSales";
+        $queryResult = @mysqli_query($DBConnect, $SQLstring)
+        Or die ("<p>Unable to query the $TableName table.</p>"."<p>Error code ". mysqli_errno($DBConnect). ": ".mysqli_error($DBConnect)). "</p>";
+
+        echo "<h1>Product Table</h1>";
+        echo "<table width='100%' border='1'>";
+        echo "<th>Item ID</th><th>Item Name</th><th>Item Description</th><th>Retail Price</th><th>Current Price</th><th>Item Cost</th><th>Stock Level</th><th>Stock Order</th><th>Category</th>";
+        $row = mysqli_fetch_row($queryResult);
+  
+        while ($row) 
+        {
+          echo "<tr><td>{$row[0]}</td>";
+          echo "<td>{$row[1]}</td>";
+          echo "<td>{$row[2]}</td>";
+          echo "<td>{$row[3]}</td>";
+          echo "<td>{$row[4]}</td>";
+          echo "<td>{$row[5]}</td>";
+          echo "<td>{$row[6]}</td>";
+          echo "<td>{$row[7]}</td>";
+          echo "<td>{$row[8]}</td></tr>";
+          $row = mysqli_fetch_row($queryResult);
+        } 
+        echo "</table>";
+        mysqli_close($DBConnect);
+?>
 </body>
 </html>
