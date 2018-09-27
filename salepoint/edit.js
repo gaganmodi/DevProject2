48d6b7c1b44410415e32e575faf7ab0fcd66554b
wassbooboo <100590129@student.swin.edu.au>
2018-09-27 17:39:31 +1000

    Edit Sales- Finalisation

--- salepoint/edit.js
+++ salepoint/edit.js
@@ -1,4 +1,6 @@
-
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function editCell(e) {
 	var ts = e.target.parentElement;
@@ -6,31 +8,45 @@ function editCell(e) {
   	var trs = t.getElementsByTagName("tr");
 	tds = t.getElementsByTagName("td"); 
 
-	var btnSave = document.createElement('button');
-  	btnSave.innerHTML = "Save";
+	var btnSave = document.createElement('input');
+	btnSave.type = "submit";
+	btnSave.name = "saving";
+  	btnSave.value = "Save";
   	btnSave.onclick = saveCell;
-	ts.appendChild(btnSave);
+	tds[8].appendChild(btnSave);
+	var hidden = document.createElement('input');
+	hidden.type = "hidden";
+	hidden.name = "item_id";
+	hidden.value = tds[0].value;
+	tds[8].appendChild(hidden);
 
 	var item_name = document.createElement('input');
 	item_name.type = "text";
+	item_name.name = "item_name";
 	item_name.value = tds[1].textContent;
 	var item_desc = document.createElement('input');
 	item_desc.type = "text";
+	item_desc.name = "item_desc";
 	item_desc.value = tds[2].textContent;
 	var item_normalprice = document.createElement('input');
 	item_normalprice.type = "text";
+	item_normalprice.name = "item_normalprice"
 	item_normalprice.value = tds[3].textContent;
 	var item_currentprice = document.createElement('input');
 	item_currentprice.type = "text";
+	item_currentprice.name = "item_currentprice";
 	item_currentprice.value = tds[4].textContent;
 	var item_cost = document.createElement('input');
 	item_cost.type = "text";
+	item_cost.name = "item_cost";
 	item_cost.value = tds[5].textContent;
 	var item_stock = document.createElement('input');
 	item_stock.type = "text";
+	item_stock.name = "item_stock";
 	item_stock.value = tds[6].textContent;
 	var item_stockorder = document.createElement('input');
 	item_stockorder.type = "text";
+	item_stockorder.name = "item_stockorder";
 	item_stockorder.value = tds[7].textContent;
   
   tds[1].appendChild(item_name);  
@@ -46,10 +62,12 @@ function editCell(e) {
   tds[6].appendChild(item_stock);  
   
   tds[7].appendChild(item_stockorder); 
+
   curr = t;
 }
 
 function saveCell() { 
+
   if(curr != undefined)
   {
     var inputs = curr.getElementsByTagName("td");
@@ -57,8 +75,24 @@ function saveCell() {
     {
       currInput = inputs[i].getElementsByTagName("input");
       currInput[0].parentElement.innerHTML = currInput[0].value;
+      //tf is equal to input value
+    }
+    for(var i = 8; i < inputs.length; i++)
+    {
+      currInput = inputs[i].getElementsByTagName("input");
+      currInput[0].parentElement.innerHTML = "<button onclick='editCell(event)'>Edit</button>";
+      //tf is equal to input value
     }
     curr = undefined;
+    var item_id = inputs[0].innerText;
+    var item_name = inputs[1].innerText;
+    var item_desc = inputs[2].innerText;
+    var item_normalprice = inputs[3].innerText;
+    var item_currentprice = inputs[4].innerText;
+    var item_cost = inputs[5].innerText;
+    var item_stock = inputs[6].innerText;
+    var item_stockorder = inputs[7].innerText;
+    window.location.href = "edit.php?item_id=" + item_id + "&item_name=" + item_name + "&item_desc=" + item_desc + "&item_normalprice=" + item_normalprice + "&item_currentprice=" + item_currentprice + "&item_cost=" + item_cost + "&item_stock=" + item_stock + "&item_stockorder=" + item_stockorder;
   }
 }
 
--- salepoint/edit.php
+++ salepoint/edit.php
@@ -12,24 +12,40 @@
           
           
           <a class="link-title" href="index.php">SalePoint</a>
-          
-                    
+        
       </header>
-       
-       <ul id="nav">
+    </div>
+       <nav>
+       <ul >
           <li><a class="active" href="#home">Display</a></li>
-          <li><a href="#news">Add</a></li>
-          <li><a href="#contact">Edit</a></li>
-          <li><a href="#about">Delete</a></li>
+          <li><a href="">Add</a></li>
+          <li><a href="">Edit</a></li>
+          <li><a href="">Delete</a></li>
         </ul>
-   </div>
-   <div>
+        </nav>
    <?php
-    $DBConnect = @mysqli_connect("feenix-mariadb.swin.edu.au", "s100590129","301196", "s100590129_db")
+   $DBConnect = @mysqli_connect("feenix-mariadb.swin.edu.au", "s100590129","301196", "s100590129_db")
         Or die ("<p>Unable to connect to the database server.</p>". "<p>Error code ". mysqli_connect_errno().": ". mysqli_connect_error()). "</p>";
-    $SQLstring = "SELECT item_id, item_name, item_desc, item_normalprice, item_currentprice, item_cost, item_stock, item_stockorder, item_category  FROM PHPSales";
+   if (isset($_GET['item_name']) && $_GET['item_name'] !='' && isset($_GET['item_desc']) && $_GET['item_desc'] !='' && isset($_GET['item_normalprice']) && $_GET['item_normalprice'] !='' && isset($_GET['item_currentprice']) && $_GET['item_currentprice'] !='' && isset($_GET['item_cost']) && $_GET['item_cost'] !='' && isset($_GET['item_stock']) && $_GET['item_stock'] !='' && isset($_GET['item_stockorder']) && $_GET['item_stockorder'] !='')
+        {
+
+          $item_id = $_GET['item_id'];
+          $item_name = $_GET['item_name'];
+          $item_desc = $_GET['item_desc'];
+          $item_normalprice = $_GET['item_normalprice'];
+          $item_currentprice = $_GET['item_currentprice'];
+          $item_cost = $_GET['item_cost'];
+          $item_stock = $_GET['item_stock'];
+          $item_stockorder = $_GET['item_stockorder'];
+
+          $SQLstring = "UPDATE PHPInventory SET item_name = '$item_name', item_desc = '$item_desc', item_normalprice = '$item_normalprice', item_currentprice = '$item_currentprice', item_cost = '$item_cost', item_stock = '$item_stock', item_stockorder = '$item_stockorder' WHERE item_id = '$item_id'";
         $queryResult = @mysqli_query($DBConnect, $SQLstring)
-        Or die ("<p>Unable to query the $TableName table.</p>"."<p>Error code ". mysqli_errno($DBConnect). ": ".mysqli_error($DBConnect)). "</p>";
+        Or die ("<p>Unable to query the $PHPInventory table.</p>"."<p>Error code ". mysqli_errno($DBConnect). ": ".mysqli_error($DBConnect)). "</p>";
+        }
+
+    $SQLstring = "SELECT item_id, item_name, item_desc, item_normalprice, item_currentprice, item_cost, item_stock, item_stockorder, item_category  FROM PHPInventory";
+        $queryResult = @mysqli_query($DBConnect, $SQLstring)
+        Or die ("<p>Unable to query the $PHPSales table.</p>"."<p>Error code ". mysqli_errno($DBConnect). ": ".mysqli_error($DBConnect)). "</p>";
 
         echo "<h1></br></br>Product Table</h1>";
         echo "<table width='100%' border='1'>";
@@ -51,17 +67,8 @@
         } 
         echo "</table></div>";
 
-        if (isset($_POST['saving']))
-        {
-          $item_name = $_GET['item_name'];
-          $item_desc = $_GET['item_desc'];
-          $item_normalprice = $_GET['item_normalprice'];
-          $item_currentprice = $_GET['item_currentprice'];
-          $item_cost = $_GET['item_cost'];
-          $item_stock = $_GET['item_stock'];
-          $item_stockorder = $_GET['item_stockorder'];
-        }
         mysqli_close($DBConnect);
 ?>
+   <div>
 </body>
 </html>
