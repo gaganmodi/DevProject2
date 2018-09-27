900f9a7fd8f9425a466c0de7304739b7b4a2e4f7
ZachKeuken <37859652+ZachKeuken@users.noreply.github.com>
2018-09-27 19:12:37 +1000

    Change Database Connection details

--- salepoint/ajax.php
+++ salepoint/ajax.php
@@ -1,7 +1,8 @@
 <?php
 	if (isset($_POST['key'])) {
 
-		$conn = new mysqli('localhost', 'root', '', 'php');
+		$conn = @mysqli_connect("feenix-mariadb.swin.edu.au", "s100590129","301196", "s100590129_db")
+			Or die ("<p>Unable to connect to the database server.</p>". "<p>Error code ". mysqli_connect_errno().": ". mysqli_connect_error()). "</p>";
 
 		if ($_POST['key'] == 'getExistingData') {
 			$start = $conn->real_escape_string($_POST['start']);
